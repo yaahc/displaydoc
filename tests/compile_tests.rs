2a494671ef4a2082358a34b0a65d78a91a40c87b
Jane Lusby <jlusby@yaah.dev>
2019-10-15 15:14:01 -0700

    Add a compile test for no_std

---
+++ tests/compile_tests.rs
@@ -0,0 +1,14 @@
+#[test]
+fn runner() {
+    no_std_test();
+}
+
+#[rustversion::nightly]
+fn no_std_test() {
+    let t = trybuild::TestCases::new();
+    t.compile_fail("tests/no_std/without.rs");
+    t.pass("tests/no_std/with.rs");
+}
+
+#[rustversion::not(nightly)]
+fn no_std_test() {}
