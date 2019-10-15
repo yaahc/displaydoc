0178606d7a9babce384d2d4f9a55bf27ff6cc68f
Jane Lusby <jlusby@yaah.dev>
2019-10-15 15:22:12 -0700

    Force tests to fail on nightly to prove ci is not broken

--- tests/compile_tests.rs
+++ tests/compile_tests.rs
@@ -8,6 +8,7 @@ fn no_std_test() {
     let t = trybuild::TestCases::new();
     t.compile_fail("tests/no_std/without.rs");
     t.pass("tests/no_std/with.rs");
+    panic!()
 }
 
 #[rustversion::not(nightly)]
