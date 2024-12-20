dfa2f8516d15def144e193f80477b5a486643d0e
Manish Goregaokar <manishsmail@gmail.com>
2024-12-20 10:33:53 -0800

    Make ui tests std-only, write separate no-std tests

--- tests/std/multi_line.rs
+++
@@ -1,1 +0,0 @@
-../no_std/multi_line.rs
---
+++ tests/ui/multi_line.rs
@@ -0,0 +1,17 @@
+use displaydoc::Display;
+
+/// this type is pretty swell
+#[derive(Display)]
+enum TestType {
+    /// This one is okay
+    Variant1,
+
+    /// Multi
+    /// line
+    /// doc.
+    Variant2,
+}
+
+static_assertions::assert_impl_all!(TestType: core::fmt::Display);
+
+fn main() {}
